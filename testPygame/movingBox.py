435a9da48883886bd03fa9e5bd74441178d23714
OlivierIrac <olivier.irac@gmail.com>
2017-06-27 10:06:18 +0200

    Improvements
    Modify ImageHandle to take file directly
    Added Windows min WIDTH and HEIGHT

--- testPygame/movingBox.py
+++ testPygame/movingBox.py
@@ -32,7 +32,9 @@ def resizeProportional (x1,y1,x2,y2):
 
 
 
-class MyWindow: 
+class MyWindow:
+    MIN_WINDOW_WIDTH=10
+    MIN_WINDOW_HEIGHT=10
     windowsList=[]
     zoomSpeed=10
     mxPress=0
@@ -42,26 +44,30 @@ class MyWindow:
     def draw():
         screen.fill((0, 0, 0))
         for window in reversed(MyWindow.windowsList):
-            window.drawFunction(window.x0,window.y0,window.width,window.height)
+            window.drawFunction(window.left,window.top,window.width,window.height)
+            if (window.decoration):
+                pygame.draw.rect(screen,colorDictionary['blue'],pygame.Rect(window.left,window.top,window.width,window.height),2)
+                
         
-    def __init__ (self, drawFunction, xLeft = 0, yTop = 0, windowWidth=800, windowHeight=600):
+    def __init__ (self, drawFunction, left = 0, top = 0, windowWidth=800, windowHeight=600, decoration=False):
         self.width=windowWidth
         self.height=windowHeight
-        self.x0=xLeft
-        self.y0=yTop
+        self.left=left
+        self.top=top
         self.drag = False
         self.resize=False
         self.drawFunction=drawFunction
         self.width0=self.width
         self.height0=self.height
-        self.x00=self.x0
-        self.y00=self.y0
+        self.left0=self.left
+        self.top0=self.top
+        self.decoration=decoration
         MyWindow.windowsList.insert(0,self)
 #        MyWindow.windowsList.append(self)
         MyWindow.draw()
     
     def isInBottomRightCorner (self,x,y):
-        return ((self.x0 + self.width -2) <= x <= (self.x0 + self.width + 5) and (self.y0 + self.height -2) <= y <= (self.y0 + self.height + 5)) 
+        return ((self.left + self.width -2) <= x <= (self.left + self.width + 5) and (self.top + self.height -2) <= y <= (self.top + self.height + 5)) 
  
     @staticmethod
     def handleWindows(event):
@@ -84,12 +90,12 @@ class MyWindow:
                         window.width0=window.width
                         window.height0=window.height
                         windowSelected=True
-                    if (window.x0<=mx<window.x0+window.width-2 and window.y0<=my<window.y0+window.height-2): #click inside shape = move
+                    if (window.left<=mx<window.left+window.width-2 and window.top<=my<window.top+window.height-2): #click inside shape = move
                         MyWindow.mxPress=mx
                         MyWindow.myPress=my               
                         window.drag=True
-                        window.x00=window.x0
-                        window.y00=window.y0
+                        window.left0=window.left
+                        window.top0=window.top
                         MyWindow.windowsList.remove(window)
                         MyWindow.windowsList.insert(0,window)
                         windowSelected=True
@@ -109,49 +115,55 @@ class MyWindow:
                 pygame.mouse.set_cursor(*pygame.cursors.arrow)
 
             if (window.drag == True):
-                window.x0 = window.x00 + mx - MyWindow.mxPress
-                window.y0 = window.y00 + my - MyWindow.myPress
+                window.left = window.left0 + mx - MyWindow.mxPress
+                window.top = window.top0 + my - MyWindow.myPress
                 
             if (window.resize == True):
                 window.width = window.width0 + mx - MyWindow.mxPress
+                if (window.width<=MyWindow.MIN_WINDOW_WIDTH):
+                    window.width=MyWindow.MIN_WINDOW_WIDTH
                 window.height = window.height0 + my - MyWindow.myPress    
-
+                if (window.height<=MyWindow.MIN_WINDOW_HEIGHT):
+                    window.height=MyWindow.MIN_WINDOW_HEIGHT
         MyWindow.draw()
+
+
+
+    
+class ImageHandle:
+    def __init__(self, file, proportional=False):
+        self.w=0
+        self.h=0
+        self.surface=pygame.image.load(file)
+        self.imgToBlit=self.surface.convert()
+        self.proportional=proportional
+        
+    def Draw (self,left,top,w,h,):
+        if (self.w!=w or self.h!=h):
+            self.w=w
+            self.h=h
+            if (self.proportional):
+                width=self.surface.get_width()
+                height=self.surface.get_height()
+                (width,height)=resizeProportional(width,height,w,h)
+            else:
+                width=w
+                height=h                
+            imgZoomed=pygame.transform.smoothscale(self.surface, (width, height))
+            self.imgToBlit=imgZoomed.convert()
+        screen.blit(self.imgToBlit, (left, top))
+
 
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
-    
-class imgHandle:
-    def __init__(self, surface):
-        self.w=0
-        self.h=0
-        self.surface=surface
-        self.imgToBlit=surface.convert()
-        
-    def Draw (self,x0,y0,w,h):
-        if (self.w!=w or self.h!=h):
-            self.w=w
-            self.h=h
-            print ("zoom")
-            imgZoomed=pygame.transform.smoothscale(self.surface, (self.w, self.h))
-            self.imgToBlit=imgZoomed.convert()
-        screen.blit(self.imgToBlit, (x0, y0))
-
-img1=pygame.image.load(os.path.join('data','2015_06_27_EOS 70D_0978.jpg'))
-img1Handle=imgHandle(img1)
-img2=pygame.image.load(os.path.join('data','alien1.jpg'))
-img2Handle=imgHandle(img2)         
-img3=pygame.image.load(os.path.join('data','IMG_0760.jpg'))
-img3Handle=imgHandle(img3)         
-
 clock = pygame.time.Clock()
+
+img1Window=MyWindow(ImageHandle(os.path.join('data','2015_06_27_EOS 70D_0978.jpg'),True).Draw,0,0,100,100,True)
+img2Window=MyWindow(ImageHandle(os.path.join('data','alien1.jpg')).Draw,50,50,150,150)
+img3Window=MyWindow(ImageHandle(os.path.join('data','IMG_0760.jpg')).Draw,100,150,200,400)
+
 done = False
-
-img1Window=MyWindow(img1Handle.Draw,0,0,100,100)
-img2Window=MyWindow(img2Handle.Draw,50,50,150,150)
-img3Window=MyWindow(img3Handle.Draw,100,150,200,400)
-
 while (not done):
     pygame.display.set_caption(" FPS : {:.4}".format(clock.get_fps()))
     
