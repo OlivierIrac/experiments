f659d6b73362ebce147278c08d43b4cc8c4499a9
Olivier <Olivier@PCOlivier>
2017-07-06 22:42:49 +0200

    Correction bug stockage fichier
    correction bug stockage fichier

--- wordGenerator/wordGenerator.py
+++ wordGenerator/wordGenerator.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 def buildLetterProbability(directory):
     letterProbability= [ [ 0 for i in range(27) ] for j in range(27) ]
     for filename in os.listdir(directory):
-        print (filename,"...", end=" ")
+        print (os.path.join(directory,filename),"...", end=" ")
         i=0
         e=0
         for line in open(os.path.join(directory,filename)):
@@ -24,16 +24,17 @@ def buildLetterProbability(directory):
                         letterProbability[string.ascii_lowercase.index(line[j])][26] +=1
             except ValueError:
                 e+=1       
-        print (i,"words parsed",e,"errors")
+        print (i,"words parsed,",e,"errors")
         
-        f = open(os.path.join(directory,"LetterProbability.csv"),"w")
-        for a in range (0,27):
-            for b in range (0,26):
-                f.write (str(letterProbability[a][b]))
-                f.write (";")
-            f.write (str(letterProbability[a][26]))
-            f.write ("\n")
-        f.close()
+    f = open(os.path.join(directory,"LetterProbability.csv"),"w")
+    for a in range (0,27):
+        for b in range (0,26):
+            f.write (str(letterProbability[a][b]))
+            f.write (";")
+        f.write (str(letterProbability[a][26]))
+        f.write ("\n")
+    f.close()
+    print ("Results stored in:", os.path.join(directory,"LetterProbability.csv"))
 #     for a in range (0,27):
 #         print (chr(97+a), end=" ")
 #         for b in range (0,27):
