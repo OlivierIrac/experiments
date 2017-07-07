c7daf0469d61d0cb2c5b71ea7ceca049341e0d70
OlivierIrac <olivier.irac@gmail.com>
2017-07-07 14:48:22 +0200

    First full version

--- wordGenerator/wordGenerator.py
+++ wordGenerator/wordGenerator.py
@@ -5,7 +5,17 @@ Created on 6 juil. 2017
 '''
 import os
 import string
-import matplotlib.pyplot as plt
+import random
+from numpy.random import choice
+
+def normalize (probaTable):
+    for i in range (27):
+        s=0
+        for j in range (27):
+            s+=probaTable[i][j]
+        for j in range (27):
+            probaTable[i][j]/=s
+    return probaTable
 
 def buildLetterProbability(directory):
     letterProbability= [ [ 0 for i in range(27) ] for j in range(27) ]
@@ -26,6 +36,8 @@ def buildLetterProbability(directory):
                 e+=1       
         print (i,"words parsed,",e,"errors")
         
+    letterProbability=normalize(letterProbability)
+        
     f = open(os.path.join(directory,"LetterProbability.csv"),"w")
     for a in range (0,27):
         for b in range (0,26):
@@ -35,14 +47,25 @@ def buildLetterProbability(directory):
         f.write ("\n")
     f.close()
     print ("Results stored in:", os.path.join(directory,"LetterProbability.csv"))
-#     for a in range (0,27):
-#         print (chr(97+a), end=" ")
-#         for b in range (0,27):
-#             print (chr(97+b),letterProbability[a][b], end=" ")
-#         print ("\n")
-#         
-#     plt.plot(letterProbability)
-#     plt.show()
-#             
-buildLetterProbability("francais")
+    
+    return letterProbability
+
+
+    
+def createRandomWord(probaLetters):
+    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+    word=""
+    newLetter=choice(alphabet, p=probaLetters[26])
+    while (newLetter!=" "):
+        word+=newLetter
+        index=string.ascii_lowercase.index(newLetter)
+        newLetter=choice(alphabet, p=probaLetters[index])
+    return word
         
+    
+# main           
+#probaLetters=buildLetterProbability("English Open Word List (EOWL)")
+probaLetters=buildLetterProbability("francais")
+random.seed()
+for i in range (100):
+    print (createRandomWord(probaLetters))
