9c8653eaaf2e5aec51a986dd232aaa55e88e80ca
OlivierIrac <olivier.irac@gmail.com>
2017-09-07 21:28:07 +0200

    Added cgi, tk and kivvy experiments

---
+++ cgi/helloWorld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+'''
+Created on 16 juil. 2017
+
+@author: Olivier
+'''
+
+print("Content-type: text/html")
+print("<html><header><title>This is title</title></header><body>Hello world</body></html>")
